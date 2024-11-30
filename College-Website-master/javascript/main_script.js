739d13322551df39ddbb8dc9ed3b1aee96fdd817
ADITYA SAROHA <aditya@localhost.localdomain>
2024-11-30 17:19:26 +0530

    Main Changes

---
+++ College-Website-master/javascript/main_script.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks");
+
+function showmenu() {
+    navLinks.style.right = "-100px";
+}
+function hidemenu() {
+    navLinks.style.right = "-800px";
+}
+
+// document.addEventListener('contextmenu', event => event.preventDefault());
